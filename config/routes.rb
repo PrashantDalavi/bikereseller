977f0e6a3635c8cb33e03389a617fb679cfd75a9
PrashantDalavi <p.d.dalavi@gmail.com>
2019-03-27 15:56:30 +0100

    5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  resources :users, only: [:new, :create, :edit, :update, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
